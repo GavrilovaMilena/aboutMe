33542cb7ad43aaf78ffa824445fced1bcf2643a4
Milena Gavrilova <mia.gggg@yandex.ru>
2022-11-26 20:06:18 +0300

    added text appearance

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        }
+    });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.title-text');
+for (let elm of elements) {
+    observer.observe(elm);
+}
+
+
+// Scroll up button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {

f5c625ddb4278985fe7a57168790912aca485f00
Milena Gavrilova <mia.gggg@yandex.ru>
2022-12-05 16:13:45 +0300

    added theme change

--- index.js
+++ index.js
@@ -23,9 +23,29 @@ function scrollFunction() {
         document.getElementById("myBtn").style.display = "none";
     }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// Add button
+const btn = document.querySelector('.theme');
+// Choosing a style sheet
+const theme = document.querySelector("#light-theme");
+// Отслеживаем щелчок по кнопке
+btn.addEventListener("click", function() {
+    // If the current address contains "index.css"
+    if (theme.getAttribute("href") === "index.css") {
+        // ...then we switch to "dark-theme.css"
+        theme.href = "dark-theme.css";
+        // Otherwise…
+    } else {
+        // …switch to "index.css"
+        theme.href = "index.css";
+    }
+});
+
+// function viewDiv(){
+//     document.getElementById("i-can").style.display = "block";
+// };

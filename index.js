e8f460b1ac59a9b1273c3fa39f4caeda21c82e72
Milena Gavrilova <mia.gggg@yandex.ru>
2022-12-06 17:16:32 +0300

    Added persistence of theme change after page reload

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function onEntry(entry) {
         }
     });
 }
-let options = { threshold: [0.5] };
+
+let options = {threshold: [0.5]};
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.title-text');
 for (let elm of elements) {
@@ -15,7 +16,10 @@ for (let elm of elements) {
 
 
 // Scroll up button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
@@ -23,31 +27,70 @@ function scrollFunction() {
         document.getElementById("myBtn").style.display = "none";
     }
 }
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////// СМЕНА ТЕМЫ
 
-// Add a change theme button
-const btn = document.querySelector('.theme');
-// Выбор таблицы стилей
-const theme = document.querySelector("#light-theme");
-// Отслеживаем щелчок по кнопке
-btn.addEventListener("click", function() {
-    // If the current address contains "index.css"
-    if (theme.getAttribute("href") === "index.css") {
-        // ...then we switch to "dark-theme.css"
-        theme.href = "dark-theme.css";
-        // Otherwise…
-    } else {
-        // …switch to "index.css"
-        theme.href = "index.css";
-    }
+// Помещаем кнопки смены темы в переменную
+let btn = document.querySelectorAll('.theme');
+
+btn.forEach(button => {
+    // К каждой добавляем обработчик событий на клик
+    button.addEventListener('click', function () {
+        // Помещаем в переменную название темы из атрибута data-theme
+        let theme = this.dataset.theme;
+        // Вызываем функцию, которая меняет тему и передаем в нее её название
+        applyTheme(theme);
+    });
 });
+function applyTheme(themeName) {
+    // Помещаем путь к файлу темы в пустой link в head
+    document.querySelector('[title="theme"]').setAttribute('href', `${themeName}-theme.css`);
+    btn.forEach(button => {
+        // Показываем все кнопки смены темы
+        button.style.display = 'block';
+    });
+    // Но скрываем кнопку для активной темы
+    document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none';
+    localStorage.setItem('theme', themeName);
+}
+// Проверяем есть ли в LocalStorage записано значение для 'theme' и присваиваем его переменной.
+let activeTheme = localStorage.getItem('theme');
+// Если значение не записано, или оно равно 'light' - применяем светлую тему
+if (activeTheme === null || activeTheme === 'light') {
+    applyTheme('light');
+    // Если значение равно 'dark' - применяем темную
+} else if (activeTheme === 'dark') {
+    applyTheme('dark');
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// function viewDiv(){
-//     document.getElementById("i-can").style.display = "block";
-// }
+// // Add a change theme button
+// const btn = document.querySelector('.theme');
+// // Выбор таблицы стилей
+// const theme = document.querySelector("#light-theme");
+// // Отслеживаем щелчок по кнопке
+// btn.addEventListener("click", function () {
+//     // If the current address contains "light-theme.css"
+//     if (theme.getAttribute("href") === "light-theme.css") {
+//         // ...then we switch to "dark-theme.css"
+//         theme.href = "dark-theme.css";
+//         localStorage.setItem("myKey", JSON.stringify(theme));
+//         // Otherwise…
+//     } else {
+//         // …switch to "light-theme.css"
+//         theme.href = "light-theme.css";
+//         localStorage.setItem("myKey", JSON.stringify(theme));
+//     }
+// });
+
+
+function viewDiv() {
+    document.getElementById("i-can").style.display = "block";
+}
